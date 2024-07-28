2a93dd9cc816cf5a6938376a14958bfb69bedf1e
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-07-28 18:24:03 +0900

    モバイル修正

--- app/javascript/cook.js
+++ app/javascript/cook.js
@@ -1,18 +1,22 @@
 
-window.addEventListener('turbo:load', function(){
+window.addEventListener('turbo:load', function() {
   const problems = document.querySelectorAll('.problem');
 
   problems.forEach(function(problem) {
     const selectButtons = problem.querySelectorAll('.select-button');
     const selectMessages = problem.querySelectorAll('.select-message');
 
-    selectButtons.forEach(function(selectButton) {
-      selectButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        selectMessages.forEach(function(selectMessage) {
-          selectMessage.style.display = 'block';
-        });
+    // handleClick 関数の定義
+    function handleClick(event) {
+      event.preventDefault();
+      selectMessages.forEach(function(selectMessage) {
+        selectMessage.style.display = 'block';
       });
+    }
+
+    // 各ボタンに handleClick 関数をイベントリスナーとして登録
+    selectButtons.forEach(function(selectButton) {
+      selectButton.addEventListener('click', handleClick);
     });
   });
 });
